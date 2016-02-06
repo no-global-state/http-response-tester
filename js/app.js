a30a1924cde2d9d9a79b46c1b15ae70107888d13
David Yang <daworld.ny@gmail.com>
2016-02-06 06:55:04 +0500

    Forced to send parameters as key => value pairs

--- js/app.js
+++ js/app.js
@@ -3,25 +3,28 @@ var app = angular.module('tester', []);
 
 app.controller('bootstrapCtrl', function($scope, $http){
     $scope.parameters = [];
-    
+
     $scope.addParameter = function(){
         // Append empty object
-        $scope.parameters.push({});
+        $scope.parameters.push({
+            name: null,
+            value: null
+        });
     }
     
     $scope.sendRequest = function(){
+        var data = $.param($scope.parameters);
+
         // Sending a request here
         $http({
-            
             method : $scope.method,
             url : $scope.url,
-            data : $("form").serialize(),
+            data : data,
             headers : {
                 'Content-Type': 'application/x-www-form-urlencoded' 
             }
             
         }).success(function(data){
-            
             console.log(data);
         });
     }
